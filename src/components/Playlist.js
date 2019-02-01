376cdaae164ad36acee7993ce45cbce3bdc8aca2
Tucker Nemcek <tuckernemcek21@gmail.com>
2019-02-01 16:08:45 -0700

    working on passing playlist info

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
+import {Link} from "react-router-dom"
 
 class Playlist extends Component {
   render(){
     return(
       <div>
         <ul>
-        <li className="list-group-item">{this.props.playlist.name} </li>
+
+        <li className="list-group-item"><Link to = "/detailedplaylist" className="btn btn-primary loginButton" onClick={this.setState({...this.state.selectedPlaylist, this.props.playlist.name}),console.log("HEY THIS ONE IS",this.state.selectedPlaylist)}>{this.props.playlist.name}</Link> </li>
         </ul>
       </div>
     )

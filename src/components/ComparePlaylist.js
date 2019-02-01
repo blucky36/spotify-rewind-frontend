b16f58c1a9fc1ffc839bbbd278568fa72dbd90a9
Paul Mourer <paul.mourer@gmail.com>
2019-02-01 16:06:48 -0700

    comparing works, probably not at all performant. includes moved tracks

---
+++ src/components/ComparePlaylist.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+const diff = require('diff')
+
+class ComparePlaylist extends Component {
+
+  state = {
+    playlist1: [
+
+    ],
+    playlist2: [
+
+    ],
+    playlist1Diff: [],
+    playlist2Diff: []
+  }
+
+  componentDidMount = async () => {
+    const playlistReq = await fetch('http://localhost:3005/api/users/1249465062/playlists/19qKzDBqnI0GKkGzU8Owcw/versions/1')
+    const playlistActual = await playlistReq.json()
+
+    const pl1 = [...playlistActual]
+    const pl2 = []
+    pl1.forEach(plItem => {
+      if (Math.random() > 0.3) {
+        pl2.push(playlistActual.splice(Math.floor(Math.random() * playlistActual.length), 1)[0])
+      }
+    })
+
+    pl1.splice(Math.floor(Math.random() * pl1.length),1)
+
+    const diffArr = diff.diffArrays(pl1, pl2)
+
+    let oldPl = []
+    let newPl = []
+
+    diffArr.forEach(diffObj => {
+      diffObj.value.forEach(val => {
+        if (diffObj.added) {
+          if (pl1.includes(val)) {
+            newPl.push({ ...val, moved: true })
+            oldPl.push({})
+          } else {
+            newPl.push({ ...val, added: true })
+            oldPl.push({})
+          }
+        } else if (diffObj.removed) {
+          newPl.push({})
+          oldPl.push({ ...val, removed: true })
+        } else {
+          newPl.push(val)
+          oldPl.push(val)
+        }
+      })
+    })
+
+    newPl.forEach(item => {
+      if (item.moved) {
+        oldPl = oldPl.map(oldItem => {
+          if (oldItem['spotify_id'] === item['spotify_id']) return { ...oldItem, moved: true }
+          return oldItem
+        })
+      }
+    })
+
+
+    this.setState(prevState => {
+      return ({
+        playlist1: pl1,
+        playlist2: pl2,
+        playlist1Diff: oldPl,
+        playlist2Diff: newPl
+      })
+    })
+
+  }
+
+  renderPlaylistDiff = (playlist) => {
+
+    return playlist.map(item => {
+        if (item.moved) {
+          item.color = 'yellow'
+        } else if (item.added) {
+          item.color = 'green'
+        } else if (item.removed) {
+          item.color = 'red'
+        } else {
+          item.color = 'black'
+        }
+        return ( < li > { item === null ? '' : <span style={{color:item.color}}>{item['spotify_id']}</span>} < /li>)
+        })
+    }
+
+    render() {
+      return ( <
+        div className = 'container' >
+        <
+        div className = 'row' >
+        <
+        div className = 'col-sm' >
+        <
+        ul > { this.renderPlaylistDiff(this.state.playlist1Diff) } <
+        /ul> < /
+        div >
+
+        <
+        div className = 'col-sm' >
+        <
+        ul > { this.renderPlaylistDiff(this.state.playlist2Diff) } <
+        /ul>
+
+        <
+        /div> < /
+        div > <
+        /div>
+
+
+      );
+    }
+
+  }
+
+  export default ComparePlaylist;

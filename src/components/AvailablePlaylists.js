56eec6ff2f40f3e10832c51c195935b1eea2257d
blucky36 <blucky36@gmail.com>
2019-02-03 10:25:47 -0700

    saturday changes

--- src/components/AvailablePlaylists.js
+++ src/components/AvailablePlaylists.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react'
-import {Link} from "react-router-dom"
 import Playlist from './Playlist'
-import Navbar from "./Navbar.js"
 
 export default class AvailablePlaylists extends Component {
 
@@ -10,26 +8,29 @@ export default class AvailablePlaylists extends Component {
     let userData = await fetch(`https://api.spotify.com/v1/users/${tokenObj.userId}`,{
       headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
     }).then(data=>data.json())
-    let playlistsData = await fetch(`https://api.spotify.com/v1/users/${tokenObj.userId}/playlists`,{
+    let playlistsData = await fetch(`https://api.spotify.com/v1/users/${tokenObj.userId}/playlists?limit=50`,{
+      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
+    }).then(data=>data.json())
+    let playlistData2 = await fetch(`https://api.spotify.com/v1/users/${tokenObj.userId}/playlists?offset=50&limit=50`,{
       headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
     }).then(data=>data.json())
     console.log(userData)
     console.log(playlistsData);
-    this.props.setMain(playlistsData,userData)
+    console.log(playlistData2);
+    this.props.setMain(playlistsData,userData,playlistData2)
   }
 
   render(){
     return(
       <div>
-      <Navbar navState = {{avatar:this.props.state.avatar,name:this.props.state.userData.display_name}}/>
-        <div>
+        <div className="row">
+          <div className="col-2"></div>
+          <div className="col-8 center-block">
+            <button type="button" className="btn btn-primary">Add a new playlist</button>
+          </div>
+          <div className="col-2"></div>
         </div>
-        <div className="col-md-4 center-block">
-          <button type="button" className="btn btn-primary">Add a new playlist</button>
-        </div>
-        <ul className="list-group">
-          {this.props.state.playlists.map((playlist,i)=><Playlist key={i} playlist = {playlist} select={this.props.selectPlaylist}/>)}
-        </ul>
+        {this.props.state.playlists.map((playlist,i)=><Playlist key={i} playlist = {playlist} select={this.props.selectPlaylist}/>)}
       </div>
     )
   }
--- src/components/Track.js
+++ src/components/Track.js
@@ -3,7 +3,7 @@ import React from "react"
 const Track = (props) => (
   <tr>
     <th scope="row">{props.track.track.id}</th>
-    <td><img src = {`${props.track.track.album.images[2].url}`} className = "track"/></td>
+    <td><img src = {props.track.track.album.images.length > 0 ? `${props.track.track.album.images[0].url}`:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png"} alt="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png" className = "track"/></td>
     <td>{props.track.track.name}</td>
     <td>{props.track.track.artists[0].name}</td>
     <td>

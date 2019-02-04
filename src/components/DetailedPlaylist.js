2b75592a49c2146fffa21dc379d189eb2f6cb219
Paul Mourer <paul.mourer@gmail.com>
2019-02-04 09:14:42 -0700

    added version of detailed playlists with while loop

--- src/components/DetailedPlaylist.js
+++ src/components/DetailedPlaylist.js
@@ -1,67 +1,46 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Track from "./Track.js"
 
 class DetailedPlayliest extends Component {
 
-  async componentDidMount(){
+  async componentDidMount() {
     let tokenObj = JSON.parse(localStorage.getItem("token"))
     let urlSplit = window.location.href.split("/")
-    let playlistId = urlSplit[urlSplit.length-1]
+    let playlistId = urlSplit[urlSplit.length - 1]
 
-    //// i think this could be cleaned up to use a for loop based on the apiResponse.total (total number of songs in a playlist)...
-    //// divide that number by 100, if there is a modulo do it that many times + 1, no modulo do it that many times....
-    //// concat/push each response onto the original repeatedly...
-    let trackData = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=0&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData2 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=100&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData3 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=200&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData4 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=300&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData5 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=400&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData6 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=500&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData7 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=600&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData8 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=700&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData9 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=800&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData10 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=900&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackArray = trackData.items.concat(trackData2.items).concat(trackData3.items).concat(trackData4.items).concat(trackData5.items).concat(trackData6.items).concat(trackData7.items).concat(trackData8.items).concat(trackData9.items).concat(trackData10.items)
-    console.log(trackArray,"playlistData")
-    console.log(this.props.state)
-    let backendUserData = await fetch(`${process.env.REACT_APP_BACKEND_API}/api/users/${tokenObj.userId}`).then(data=>data.json())
-    await fetch(`${process.env.REACT_APP_BACKEND_API}/api/users/${backendUserData.id}/playlists`,
-      {
-        method:"post",
-        headers: {"Content-Type": "application/json","Accept": "application/json"},
-        body:JSON.stringify({
-          spotify_playlist_id:this.props.state.selected.id,
-          name:this.props.state.selected.name,
-          notes:"oibruv",
-          trackArray:trackArray
-        })
-      }
-    )
+    let remainingLength = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=0&limit=100`, {
+      headers: { "Content-Type": "application/json", "Authorization": `Bearer ${tokenObj.accessToken}` }
+    }).then(data => data.json()).then(data => data.total)
+
+    console.log(remainingLength)
+
+    const trackArray = []
+    let offset = 0
+
+    do {
+      const nextTracks = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=${offset*100}&limit=100`, {
+        headers: { "Content-Type": "application/json", "Authorization": `Bearer ${tokenObj.accessToken}` }
+      }).then(data => data.json())
+      trackArray.push(...nextTracks.items)
+      remainingLength = remainingLength - 100
+    } while (remainingLength > 0)
+
+    let backendUserData = await fetch(`${process.env.REACT_APP_BACKEND_API}/api/users/${tokenObj.userId}`).then(data => data.json())
+    await fetch(`${process.env.REACT_APP_BACKEND_API}/api/users/${backendUserData.id}/playlists`, {
+      method: "post",
+      headers: { "Content-Type": "application/json", "Accept": "application/json" },
+      body: JSON.stringify({
+        spotify_playlist_id: this.props.state.selected.id,
+        name: this.props.state.selected.name,
+        notes: "oibruv",
+        trackArray: trackArray
+      })
+    })
     this.props.grabTracks(trackArray)
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <div className="row">
           <div className="col-2">

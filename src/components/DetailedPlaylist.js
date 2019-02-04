7d337bd2d8a129013d670c07eed72dc282930012
blucky36 <blucky36@gmail.com>
2019-02-04 08:39:25 -0700

    MERGE CONFLICTS my take on refactoring long playlist calls

--- src/components/DetailedPlaylist.js
+++ src/components/DetailedPlaylist.js
@@ -10,49 +10,39 @@ class DetailedPlayliest extends Component {
     let trackData = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=0&limit=100`,{
       headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
     }).then(data=>data.json())
-    let trackData2 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=100&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData3 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=200&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData4 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=300&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData5 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=400&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData6 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=500&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData7 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=600&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData8 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=700&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData9 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=800&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackData10 = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=900&limit=100`,{
-      headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
-    }).then(data=>data.json())
-    let trackArray = trackData.items.concat(trackData2.items).concat(trackData3.items).concat(trackData4.items).concat(trackData5.items).concat(trackData6.items).concat(trackData7.items).concat(trackData8.items).concat(trackData9.items).concat(trackData10.items)
-    console.log(trackArray,"playlistData")
-    console.log(this.props.state)
-    let backendUserData = await fetch(`${process.env.REACT_APP_BACKEND_API}/api/users/${tokenObj.userId}`).then(data=>data.json())
-    await fetch(`${process.env.REACT_APP_BACKEND_API}/api/users/${backendUserData.id}/playlists`,
-      {
-        method:"post",
-        headers: {"Content-Type": "application/json","Accept": "application/json"},
-        body:JSON.stringify({
-          spotify_playlist_id:this.props.state.selected.id,
-          name:this.props.state.selected.name,
-          notes:"oibruv",
-          trackArray:trackArray
-        })
+    let trackArray = trackData.items
+    if(trackData.total > 100){
+      let str = trackData.total.toString(),len
+      if(trackData.total >= 100){
+        len = str.split("")[0]
       }
-    )
+      if(trackData.total >= 1000){
+        len = str.split("")[0]+str.split("")[1]
+      }
+      if(trackData.total >= 10000){
+        len = str.split("")[0]+str.split("")[1]+str.split("")[2]
+      }
+      for(let i = 1; i <= Number(len); i++){
+        trackData = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=${i}00&limit=100`,{
+          headers:{"Content-Type":"application/json","Authorization":`Bearer ${tokenObj.accessToken}`}
+        }).then(data=>data.json())
+        trackArray = trackArray.concat(trackData.items)
+      }
+    }
+    console.log(trackArray,"playlistData")
+    let backendUserData = await fetch(`${process.env.REACT_APP_BACKEND_API}/api/users/${tokenObj.userId}`).then(data=>data.json())
+    // await fetch(`${process.env.REACT_APP_BACKEND_API}/api/users/${backendUserData.id}/playlists`,
+    //   {
+    //     method:"post",
+    //     headers: {"Content-Type": "application/json","Accept": "application/json"},
+    //     body:JSON.stringify({
+    //       spotify_playlist_id:playlistId,
+    //       name:this.props.state.selected.name,
+    //       notes:"oibruv",
+    //       trackArray:trackArray
+    //     })
+    //   }
+    // )
     this.props.grabTracks(trackArray)
   }
 

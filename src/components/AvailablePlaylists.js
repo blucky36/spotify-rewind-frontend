0a3072c3aeada3531fd4721e26212eddbbb57c84
blucky36 <blucky36@gmail.com>
2019-02-07 15:04:02 -0700

    fixed spinner spacing

--- src/components/AvailablePlaylists.js
+++ src/components/AvailablePlaylists.js
@@ -9,7 +9,7 @@ export default class AvailablePlaylists extends Component {
 
   noPlaylists = () => {
     if (this.props.state.awaitingAvailable) {
-      return (<div style={{width:'100%', marginTop:10}} className='d-inline-flex p-2 justify-content-center'><div className='fidget'></div></div>)
+      return (<div style={{width:'100%', marginTop:10, marginLeft:150}}><div className='fidget'></div></div>)
     } else {
       return (<div style={{width:'100%', marginTop:10, fontStyle:'italic'}} className='d-inline-flex p-2 justify-content-center'><h4>You have no playlists to back up!</h4></div>)
     }
@@ -20,9 +20,7 @@ export default class AvailablePlaylists extends Component {
       <div>
         <div className="row">
           <div className="col-2"></div>
-          <div className="col-2">
-            <button type="button" className="btn btn-primary">Add a new playlist</button>
-          </div>
+          <div className="col-2"></div>
           <div className = "col-6"><h3>Choose a playlist you want to backup</h3></div>
           <div className="col-2"></div>
         </div>
